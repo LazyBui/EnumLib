f21367d858393e7342103be67b7cc0f01e0fa33d
LazyBui <Lazy.Bui@Gmail.com>
2016-03-23 10:58:19 -0500

    Removing bunk InternalsVisibleToAttribute

--- EnumLib/Properties/AssemblyInfo.cs
+++ EnumLib/Properties/AssemblyInfo.cs
@@ -2,8 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: InternalsVisibleTo("BoringCardLib.Tests")]
-
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
