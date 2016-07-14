5525a0106b01eb12822a227733458ca11f527a01
LazyBui <Lazy.Bui@Gmail.com>
2016-07-14 14:00:52 -0500

    Cleaning up usings

--- EnumLib/EnumExtensions.Flags.cs
+++ EnumLib/EnumExtensions.Flags.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Reflection;
 
 namespace System {
 	public static partial class EnumExtensions {
--- EnumLib/EnumExtensions.Validation.cs
+++ EnumLib/EnumExtensions.Validation.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Reflection;
 
 namespace System {
 	public static partial class EnumExtensions {
